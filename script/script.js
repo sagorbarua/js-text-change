a4b7690189d2be79509f7b569bfe4e76ad63303a
Sagor Barua <sagor102000@gmail.com>
2024-03-25 00:15:51 +0600

    js

--- script/script.js
+++ script/script.js
@@ -2,3 +2,57 @@ function changeText(){
   document.getElementById('txt-cng').innerHTML =
     "My name is Sagor Barua";
 }
+
+function bulbOn(){
+  document.getElementById('image_cng').src ='images/pic_bulbon.gif';
+
+}
+
+function bulbOff(){
+  document.getElementById('image_cng').src ='images/pic_bulboff.gif';
+
+}
+
+function sum(){
+ let num1, num2, sum;
+ num1 = 50;
+ num2 = 20;
+ sum = num1 + num2;
+
+ document.getElementById("sum").innerHTML= sum ;
+}
+
+function sub(){
+  let num1, num2, sub;
+  num1 = 50;
+  num2 = 20;
+  sub = num1 - num2;
+ 
+  document.getElementById("sub").innerHTML= sub ;
+ }
+
+ function mul(){
+  let num1, num2, mul;
+  num1 = 50;
+  num2 = 20;
+  mul = num1 * num2;
+ 
+  document.getElementById("mul").innerHTML= mul ;
+ }
+
+
+ function div(){
+  let num1, num2, div;
+  num1 = 50;
+  num2 = 5;
+  div = num1 / num2;
+ 
+  document.getElementById("div").innerHTML= div ;
+ }
+
+ function myFunction(x) {
+  x.style.background = "yellow";
+ }
+
+
+
